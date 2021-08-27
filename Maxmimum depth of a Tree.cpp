bffb2a99b8d54dc820e607229ce5889d314fcd0a
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-27 10:49:44 -0700

    Update Maxmimum depth of a Tree.cpp

--- Maxmimum depth of a Tree.cpp
+++ Maxmimum depth of a Tree.cpp
@@ -1,9 +1,13 @@
+// ======== MOST IMPORTANT USED IN MANY PROBLEMS =============
+
 
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
     if(root == NULL) return 0;
-    return 1+max(maxDepth(root->left),maxDepth(root->right));
+     int leftHeight =  maxDepth(root->left);
+     int rightHeight = maxDepth(root->right);
+    return 1 + maxDepth(leftHeight , rightHeight);
     }
 };
 
