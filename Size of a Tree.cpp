577b2ba77e1e467917d2403358c53a5efc199cd6
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-27 10:45:04 -0700

    Update Size of a Tree.cpp

--- Size of a Tree.cpp
+++ Size of a Tree.cpp
@@ -1,7 +1,11 @@
 /*============== FIND THE SIZE OF A TREE ====================*/
 
 // SIZE OF A TREE == SIZE OF A LEFT SUB-TREE + SIZE OF RIGHT SUB-TREE + 1(ROOT);
+//SIZE OF TREE MEANS = HOW MAY NUMBER OF NODES ARE THERE IN A WHOLE TREE
 int sizeOfTree(Node *root){
     if(root == NULL) return 0;
-    return sizeOfTree(root->left)+sizeOfTree(root->right)+1;
+    int NumberOfNodesInLeft = sizeOfTree(root->left);
+    int NumberOfNodesInRight = sizeOfTree(root->right);
+    
+    return NumberOfNodesInLeft + NumberOfNodesInRight + 1; //added one for root
 }
