c34942b5ad50291d8285030697cc9a1a97b1f331
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-09-01 13:20:37 -0700

    Create Right View Recursive.cpp

---
+++ Right View Recursive.cpp
@@ -0,0 +1,15 @@
+void helperFunc(TreeNode *root,int level,vector<int> &ans){
+        if(!root) return;
+        if(level== size(ans)) ans.push_back(root->val);
+        helperFunc(root->right,level+1,ans);
+        helperFunc(root->left,level+1,ans);
+
+}
+vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helperFunc(root,0,res);
+        return res;
+    }
+// TC: O(N)
+// SC: O(N)
+// for more explanation i prefer u to see this for once : https://www.youtube.com/watch?v=KV4mRzTjlAk
