92ac2966280600bb8d0c846107eabe60ca15a4f1
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-09-05 11:13:05 -0700

    Update Find the Path Of  Every Leaf node from root.cpp

--- Find the Path Of  Every Leaf node from root.cpp
+++ Find the Path Of  Every Leaf node from root.cpp
@@ -5,7 +5,7 @@ return !root->left and !root->right;
 void givePaths(TreeNode *root,string store, vector<string> &res){
 if(!root) return;
 if(isLeafNode(root)) {
-    res.push_back(store + to_string(root->val));  //when we get the leaf node we can add 1 path (string) intto the vector of string
+    res.push_back(store + to_string(root->val));  //when we get the leaf node we can add 1 path (string) into the vector of string
 }
 givePaths(root->left,store + to_string(root->val)+"->", res);
 givePaths(root->right,store + to_string(root->val)+"->", res);
@@ -16,3 +16,27 @@ givePaths(root->right,store + to_string(root->val)+"->", res);
  givePaths(root,"",ans);
  return ans;
     }
+
+
+..//in vector form
+
+bool isLeafNode(Node *root){
+    return (!root->left and !root->right);
+}
+void givePaths(Node *root, vector<int> &store, vector<vector<int>> &res) {
+    if(!root) return;
+        store.push_back(root->val);
+    if(isLeafNode(root)) {
+          res.push_back(store);//when we get the leaf node we can add 1 path (string) into the vector of string
+    }
+    givePaths(root->left, store, res);
+    givePaths(root->right, store, res);
+    store.pop_back();//this is the main step this is used to backtrack the entire traversal helps to remove unwanted values;
+}
+
+vector<vector<int>> binaryTreePaths(Node *root) {
+    vector<int> ans;
+    vector<vector<int>>res;
+    givePaths(root,ans, res);
+    return res;
+}
